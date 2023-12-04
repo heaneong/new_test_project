6cc6d262668f20b317aeed641e8fb6ec5397171e
neong0511@gmail.com <neong0511@gmail.com>
2023-12-04 15:23:38 +0900

    init-jar first commit

---
+++ src/test/java/com/example/initjar/InitJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.initjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
